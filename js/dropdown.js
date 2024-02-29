3dc9462bfb2312eea0291150d62f6250a4002354
HOANGHUY\admin <hoanghuybaw@gmail.com>
2024-02-29 22:16:33 +0700

    update ui final 29/2/2024

---
+++ js/dropdown.js
@@ -0,0 +1,21 @@
+function toggleDropdown() {
+  var dropdown = document.getElementById("myDropdown");
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+  }
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.style.display === "block") {
+        openDropdown.style.display = "none";
+      }
+    }
+  }
+};
